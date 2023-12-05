90fbd485c0efcb77857bb94647a5193945e6cab4
HehBot <razor7827@gmail.com>
2023-12-06 04:01:36 +0530

    Fixed bug in eeprom programmer Serial output

--- eeprom_programmer/eeprom_programmer.ino
+++ eeprom_programmer/eeprom_programmer.ino
@@ -39,7 +39,7 @@ void printContents(int start, int end)
             data[offset] = readEEPROM(base + offset);
 
         char buf[80];
-        sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x", base << 4, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
+        sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x", base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
 
         Serial.println(buf);
     }
